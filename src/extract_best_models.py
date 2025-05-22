413e447633b3b9b6c5cb5d47ccae0b61ccb17f3f
Divanshu-Bansal <divanshubansal05@gmail.com>
2025-05-22 16:04:04 +1000

    initial commit

---
+++ src/extract_best_models.py
@@ -0,0 +1,27 @@
+import argparse
+import os
+import shutil
+from clearml import Task, StorageManager
+
+def main(args):
+    task = Task.current_task()
+    model_selection_task = Task.get_task(task_id=args["model_selection_id"])
+
+    # Download the artifact named "best_model"
+    model_path = model_selection_task.artifacts["best_model"].get_local_copy()
+
+    # Copy it to a deployable path
+    os.makedirs(os.path.dirname(args["output_model_path"]), exist_ok=True)
+    shutil.copy(model_path, args["output_model_path"])
+    print(f"Best model saved to: {args['output_model_path']}")
+
+if __name__ == "__main__":
+    task = Task.init(project_name="AI for Diabetes Prediction", task_name="Template - Extract Best Model")
+    task.execute_remotely()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_selection_id", type=str, required=True)
+    parser.add_argument("--output_model_path", type=str, default="deployed_models/best_model.joblib")
+    args = vars(parser.parse_args())
+
+    main(args)
